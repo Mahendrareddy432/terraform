b64a21583d215506025852514496ab5f4236c5de
mahendra <eedalamahendra@gmail.com>
2022-03-19 16:02:57 +0530

    hi

--- output.tf
+++ output.tf
@@ -17,3 +17,59 @@ variable "efg" {
 output  "efg" {
   value=var.efg
 }
+
+# output "abc" {
+#  value = "Hello\nWorld"
+# }
+
+#variable "abc" {
+# default="100"
+#}
+#output "abc" {
+# value =var.abc
+#}
+
+#variable "abc1" {
+#}
+
+#output "abc1" {
+# value = var.abc1
+#}
+
+variable "abc2" {
+  default = 200
+}
+output "abc2" {
+  value ="value of abc2=${var.abc2}"
+}
+
+variable "samplex" {
+  default = [
+    "Hello",
+    100,
+    true
+  ]
+}
+
+output "samplex" {
+  value = "value 1 =${var.samplex[0]},value 3=${var.samplex[2]}"
+}
+
+variable "sampley" {
+  default = {
+    string  = "Hello",
+    number  = 100,
+    boolean = true
+  }
+}
+
+output "sampley" {
+  value = "String = ${var.sampley["string"]},Number = ${var.sampley["number"]},Boolean=${var.sampley["boolean"]}"
+}
+
+variable "a" {}
+
+output "a" {
+  value = var.a
+
+}
