b400739232147ef84964c75a799df7e40a750f33
mahendra <eedalamahendra@gmail.com>
2022-03-19 15:23:38 +0530

    hello

--- output.tf
+++ output.tf
@@ -2,18 +2,18 @@
 #  value= aws_instance
 #}
 
-#output "sample" {
-#  value= "Hello World"
-#}
-#
-#output "abc" {
-#  value= "123"
-#}
+output "sample" {
+  value= "Hello World"
+}
+
+output "abc" {
+  value= "123"
+}
 
 variable "efg" {
   default="100"
 }
 
-output  "abc" {
+output  "efg" {
   value=var.efg
 }
