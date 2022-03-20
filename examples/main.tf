5b0b6898c72d95dfa97a833799fd7ea20a9583ee
mahendra <eedalamahendra@gmail.com>
2022-03-20 19:23:13 +0530

    ec2

--- examples/main.tf
+++ examples/main.tf
@@ -9,7 +9,8 @@ region ="us_east_1"
 resource "aws_instance" "sample" {
     ami    = "ami-0bb6af715826253bf"
 instance_type ="t2.micro"
-
+vpc_security_group_ids=[aws_security_group.allow_sample.id]
+##security              providername        localname
 tags = {
     Name = "sample"
   }
