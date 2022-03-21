9d8dd71f2eafd8c2ae1f39a764577da01c624468
mahendra <eedalamahendra@gmail.com>
2022-03-21 18:29:13 +0530

    k

--- examples/ta.tf
+++ examples/ta.tf
@@ -2,7 +2,7 @@
 resource "aws_instance" "web" {
   ami           = "ami-0bb6af715826253bf"
   instance_type = "t2.micro"
-
+vpc_security_group_ids = [aws_security_group.mongo.id]
   tags = {
     Name = "Hello"
   }
