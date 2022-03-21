4faf5bd6f45ed9d82a3b1bbf707da279632a165b
mahendra <eedalamahendra@gmail.com>
2022-03-21 17:53:21 +0530

    mahe

---
+++ examples/ta.tf
@@ -0,0 +1,34 @@
+resource "aws_instance" "data" {
+    ami = "0bb6af715826253bf"
+  vpc_security_group_ids = [aws_security_group.mongo.id]
+  tags = {
+       Name = "mongodb"
+  }
+}
+
+resource "aws_security_group" "mongo" {
+  name        = "sql"
+  description = "Allow TLS inbound traffic"
+ # vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = [0.0.0.0/0]
+    ipv6_cidr_blocks = []
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "mongo"
+  }
+}
