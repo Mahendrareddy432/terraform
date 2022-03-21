8046e6c05319f6d262761b6028bca3687326f764
mahendra <eedalamahendra@gmail.com>
2022-03-21 18:14:01 +0530

    oo

--- examples/ta.tf
+++ examples/ta.tf
@@ -1,8 +1,7 @@
 
-
-resource "aws_instance"  "station" {
+ resource "aws_instance"  "station" {
     ami = "ami-0bb6af715826253bf"
-instance_type = "t3.micro"
+ instance_type = "t2.micro"
   vpc_security_group_ids = [aws_security_group.mongo.id]
 
   tags = {
