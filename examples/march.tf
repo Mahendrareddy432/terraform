f743a7521f14197dc740aa9abcc96d3ad27efa09
mahendra <eedalamahendra@gmail.com>
2022-03-27 13:14:58 +0530

    ms

---
+++ examples/march.tf
@@ -0,0 +1,37 @@
+resource "aws_instance" "devops" {
+  ami           = ami-0eb5f3f64b10d3e0e
+  instance_type = "t2.micro"
+  vpc_security_group_ids =["aws_security_group.march.id"] 
+
+  tags = {
+    Name = "welcome"
+  }
+}
+
+
+resource "aws_security_group" "march" {
+  name        = "march"
+  description = "Allow TLS inbound traffic"
+  #vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = [0.0.0.0/0]
+    ipv6_cidr_blocks = []
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "world"
+  }
+}
