d0368005e7ecf90f9c595e33d2c4f1a85d8bc641
mahendra <eedalamahendra@gmail.com>
2022-03-27 14:23:36 +0530

    hg

--- examples/march.tf
+++ examples/march.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "devops" {
-  ami           = ami-0eb5f3f64b10d3e0e
+  ami           = "ami-0eb5f3f64b10d3e0e"
   instance_type = "t2.micro"
   vpc_security_group_ids =["aws_security_group.march.id"] 
 
