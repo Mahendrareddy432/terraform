1d669f79761d1652f8694fc3121ecc5411986b3e
mahendra <eedalamahendra@gmail.com>
2022-03-27 14:57:46 +0530

    ii

--- examples/main.tf
+++ examples/main.tf
@@ -37,7 +37,7 @@ resource "aws_security_group" "work1" {
     to_port          = 0
     protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
+    ipv6_cidr_blocks = []
   }
 
   tags = {
