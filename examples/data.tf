473364fed9557d7f9d6cbc183eb9fb409a247a0e
mahendra <eedalamahendra@gmail.com>
2022-03-27 15:02:42 +0530

    kkk

--- examples/data.tf
+++ examples/data.tf
@@ -18,31 +18,31 @@
 
 # ## Security group
 
-# resource "aws_security_group" "allow_sample2" {
-#   name        = "allow_sample2"
-#   description = "Allow TLS inbound traffic"
-#   #vpc_id      = aws_vpc.main.id
+resource "aws_security_group" "allow_sample2" {
+  name        = "allow_sample2"
+  description = "Allow TLS inbound traffic"
+  #vpc_id      = aws_vpc.main.id
 
-#   ingress {
-#     description      = "TLS from VPC"
-#     from_port        = 22
-#     to_port          = 22
-#     protocol         = "tcp"
-#     cidr_blocks      = ["0.0.0.0/0"]
-#     ipv6_cidr_blocks = []
-#   }
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = []
+  }
 
-#   egress {
-#     from_port        = 0
-#     to_port          = 0
-#     protocol         = "-1"
-#     cidr_blocks      = ["0.0.0.0/0"]
-#     ipv6_cidr_blocks = ["::/0"]
-#   }
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
 
-#   tags = {
-#     Name = "allow_sample2"
-#   }
-# }
+  tags = {
+    Name = "allow_sample2"
+  }
+}
 
 
